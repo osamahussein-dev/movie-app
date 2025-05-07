2d504b75e2dc1a4cef066283b0681f4ec6de0695
osamahussein-dev <osamahusseindev@gmail.com>
2025-05-07 20:36:55 +0300

    Add Home.jsx code and style it and change the footer to position fixed in the footer.css

--- src/App.js
+++ src/App.js
@@ -16,6 +16,15 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/movies" element={<Movies />} />
           <Route path="/movies/:id" element={<MovieDetails />} />
+          <Route
+            path="*"
+            element={
+              <h1>
+                <strong>404 Not Found</strong>
+              </h1>
+            }
+          />
+
           <Route path="*" element={<h1>404 Not Found</h1>} />
         </Routes>
       </main>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,14 @@
-function Footer() {}
+import "../css/footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>&copy; Rights Reserved To Osama Hussein.</p>
+      <p>
+        <a href="https://www.themoviedb.org/">Powered by TMDb API</a>
+      </p>
+    </footer>
+  );
+}
 
 export default Footer;
